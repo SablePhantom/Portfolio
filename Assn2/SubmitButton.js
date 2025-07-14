a0d18b99df2386787f3c66e617a28872dce2fae4
Randall Lim <randalllim2000@gmail.com>
2025-07-14 23:00:15 +0800

    Add files via upload

---
+++ Assn2/SubmitButton.js
@@ -0,0 +1,13 @@
+<html>
+<body>
+<script>
+function loginConfirmed {
+  window.location.href="file:///C:/Users/Randall%20Lim/OneDrive/Documents/ComSci/FWP/Assn2/LoggedIn.html"
+}
+
+function submitConfirmed() {
+  window.location.href="file:///C:/Users/Randall%20Lim/OneDrive/Documents/ComSci/FWP/Assn2/Rented.html"
+}
+</script>
+</body>
+</html>
